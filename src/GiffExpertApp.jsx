87eed48277173a123ddee554044909a3a549a98d
Daniel Mateu <danielmateu86@gmail.com>
2022-06-21 12:18:42 +0200

    Primer Commit

---
+++ src/GiffExpertApp.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import { AddCategory, GifGrid } from './components';
+
+export const GiffExpertApp = () => {
+
+  const [categories, setCategories] = useState(['Goku']);
+
+  const onAddCategory = (newCategory) => {
+
+    if (categories.includes(newCategory)) return;
+
+    // setCategories(cat => ['Nuk',...cat]);
+    setCategories([newCategory, ...categories])
+  }
+
+  return (
+    <>
+      <h1>GiffExpertApp</h1>
+      <AddCategory
+        // setCategories = {setCategories}
+        onNewCategory={event => onAddCategory(event)}
+      />
+      {
+        categories.map((category) =>
+        (
+          <GifGrid key={category} category={category} />
+        )
+        )
+      }
+      {/* Gif Item */}
+    </>
+  )
+}
---
+++ src/components/GifItem.jsx
@@ -0,0 +1,12 @@
+import './GifItem.css';
+
+export const GifItem = ({url,title}) => {
+    return (
+        <>
+            <figure className="card">
+                <img src={url} alt={title} />
+                <figcaption>{title}</figcaption>
+            </figure>
+        </>
+    )
+}
