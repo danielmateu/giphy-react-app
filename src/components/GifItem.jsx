8764683bdb4fd44abd7dbe66a25d2c15410e6b08
Daniel Mateu <danielmateu86@gmail.com>
2022-06-21 13:59:22 +0200

    Primer Commit

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -3,7 +3,7 @@ import './GifItem.css';
 export const GifItem = ({url,title}) => {
     return (
         <>
-            <figure className="card ">
+            <figure className="card animate__animated animate__fadeInDown">
                 <img src={url} alt={title} />
                 <figcaption>{title}</figcaption>
             </figure>
