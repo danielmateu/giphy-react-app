03ea5c6cb1803373c50e6c35d9be341270b7bd06
Daniel Mateu <danielmateu86@gmail.com>
2022-08-26 14:48:14 +0200

    Test: Debe de llamar onNewCategory si el input tiene un valor

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -1,15 +1,33 @@
 import { fireEvent, render, screen } from "@testing-library/react"
 import { AddCategory } from "../../src/components/AddCategory"
 
+const inputValue = 'Saitama';
 
 describe('Pruebas en <AddCategory>', () => { 
+
     test('Debe de cambiar el valor de la caja de texto', () => { 
         render(<AddCategory onNewCategory={ () => {} }/>);
         const input = screen.getByRole('textbox');
-        fireEvent.input(input, {target: {value: 'Saitama'}});
+        fireEvent.input(input, {target: {value: inputValue}});
 
         // screen.debug();
-        expect(input.value).toBe('Saitama');
+        expect(input.value).toBe(inputValue);
+
+    });
+
+    test('Debe de llamar onNewCategory si el input tiene un valor', () => {
+        //TODO: ????
+
+        render(<AddCategory onNewCategory={ () => {} }/>);
+
+        const input = screen.getByRole('textbox');
+        fireEvent.input(input, {target: {value: inputValue}});
+
+        const form = screen.getByRole('form');
+        fireEvent.submit(form);
+        // screen.debug();
+        expect(input.value).toBe('');
+
 
     })
 })
